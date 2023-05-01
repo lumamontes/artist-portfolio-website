9934c75caa62e4a73258b9489df3d32436931f7d
Luma Montes <lumagoesmontes@gmail.com>
2023-05-01 07:29:44 -0500

    fix: add title to document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <title>Luana Góes</title>
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
